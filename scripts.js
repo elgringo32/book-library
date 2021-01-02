f0dc7f49d02ebe01f0f2097708397aa2f5b40a30
elgringo32 <bknopke@gmail.com>
2021-01-01 20:42:27 -0800

    added form and event listeners

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  var title = document.querySelectorAll('input');
+  console.log(title);
 }
+
+document.getElementById('add-book-submit').addEventListener('click', addBookToLibrary);
