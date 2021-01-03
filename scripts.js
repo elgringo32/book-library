2bb0aafdea4467f4edf9fb8ce95c38977271f182
elgringo32 <bknopke@gmail.com>
2021-01-02 19:43:26 -0800

    add card handling base code

--- scripts.js
+++ scripts.js
@@ -13,7 +13,39 @@ function addBookToLibrary() {
   addBookSubmitInput = Array.from(addBookSubmitInput).reduce((acc,input) => ({...acc,[input.id]:input.value}),{});
   myLibrary.push(addBookSubmitInput);
   console.log(myLibrary);
+
+  buildCard(addBookSubmitInput);
+  // myLibrary.forEach(book => buildCard(book));
+}
+
+document.getElementById('add-book-submit').addEventListener('click', addBookToLibrary);
+
+function buildCard(item) {
+    var divBooks = document.querySelector('#all-cards');
+    
+    var divCard = document.createElement('div')
+    divCard.classList.add('card');
+    divBooks.appendChild(divCard);
+    
+    var divCardBody = document.createElement('div');
+    divCardBody.classList.add('card-body');
+    divCard.appendChild(divCardBody);
+
+    var divCardTitle = document.createElement('h5');
+    divCardTitle.classList.add('card-title');
+    divCardTitle.innerHTML = item['book-name'];
+    divCardBody.appendChild(divCardTitle);
+
+    var divCardSubTitle = document.createElement('h6');
+    divCardSubTitle.classList.add('card-subtitle','mb-2','text-muted');
+    divCardSubTitle.innerHTML = item['book-author'];
+    divCardBody.appendChild(divCardSubTitle);
+    
+
+    // var divCardText = document.createElement('div').classList.add('card-text');
+    // var divCardDelete = document.createElement('a').classList.add('card-link');
+
 }
 
 
-document.getElementById('add-book-submit').addEventListener('click', addBookToLibrary);
+
